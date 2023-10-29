2e6bee61763fc0a09581040ecf584fb047486183
NORH123 <amnor167@gmail.com>
2023-10-29 22:41:32 +0300

    first commit

---
+++ for.php
@@ -0,0 +1,11 @@
+<?php
+$family =['Norh','naif','Majed'];
+
+foreach($family as $member){
+    echo $member .'<br>';
+}
+// for ($i=0; $i <count($family) ; $i++) { 
+//     echo $family[$i].'<br>';
+// }
+
+?>
---
+++ if.php
@@ -0,0 +1,28 @@
+<?php 
+$username= 'Norh';
+$email ='nh@gmail.com';
+if ($username !== "Nour Homsi" And $email ==='nh@gmail.com') {
+   echo 'Welcome Admin: Norh';
+}
+?>
+
+<?php 
+// $number1=1;
+// $number2=1;
+// if ($number1>$number2) {
+//    echo 'الرقم الاول اكبر ';
+//    if ($number1===1) {
+//     echo '<br>';
+//     echo 'الرقم الاول عشرون ';
+//    }
+// }elseif ($number1<$number2){
+//     echo 'الرقم الاول اصغر ';
+// } 
+   
+//  else {
+//     echo 'الرقمان  متساويان ';
+// }
+
+?>  
+
+
